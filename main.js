6ceee0e347ee507e46110f5a5e5ec10869192c2b
olakunle_exdee <saheedogunsolu2016@gmail.com>
2020-11-18 21:36:33 +0100

    animation

--- main.js
+++ main.js
@@ -12,3 +12,34 @@ hamburger.addEventListener("click", () => {
     showMenu = false;
   }
 });
+
+window.sr = ScrollReveal();
+
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+  reset: true,
+});
+
+sr.reveal(".animate-bottom", {
+  origin: "top",
+  distance: "25rem",
+  duration: 1000,
+  reset: true,
+});
