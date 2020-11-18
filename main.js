9e97aeaecff79930dfd442b6f080782c61aab835
olakunle_exdee <saheedogunsolu2016@gmail.com>
2020-11-18 02:18:47 +0100

    sticky header

--- main.js
+++ main.js
@@ -12,20 +12,3 @@ hamburger.addEventListener("click", () => {
     showMenu = false;
   }
 });
-
-const scrl = ScrollReveal({
-  origin: "top",
-  distance: "80px",
-  duration: 2000,
-  reset: true,
-});
-
-scrl.reveal(".link", { delay: 200, easing: "ease-in" });
-
-scrl.reveal(".heading", {});
-scrl.reveal(".text", { delay: 200 });
-scrl.reveal(".btn", { delay: 200 });
-scrl.reveal(".sub-content", { delay: 400 });
-scrl.reveal(".heading-group", { delay: 500 });
-scrl.reveal(".content-img", { delay: 500 });
-scrl.reveal(".content-wrapper", { delay: 300, easing: "ease-in" });
