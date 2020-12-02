f8bb345b13df5c6e26119c02c7eb185afd885f30
olakunle_exdee <saheedogunsolu2016@gmail.com>
2020-12-02 21:36:02 +0100

    gsap

--- main.js
+++ main.js
@@ -4,32 +4,53 @@ const links = document.querySelectorAll(".link");
 
 gsap.registerPlugin(ScrollTrigger);
 
-const tl = gsap.timeline();
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".hero-heading",
+    start: "top 25%",
+    end: "bottom 100%",
+  },
+});
 
-tl.from(".hero-heading", { x: "300rem", duration: 3, ease: "expo.out" })
+tl.from(".hero-heading", {
+  opacity: 0,
+  y: -30000,
+  duration: 1,
+  ease: "expo.out",
+})
   .from(".hero-text", {
-    x: "-300rem",
+    opacity: 0,
+    duration: 1,
+    x: "1000",
+    ease: "expo.out",
+  })
+  .from(".button", { opacity: 0, stagger: 1, duration: 2, ease: "expo.out" })
+  .from(".hero-image", {
+    opacity: 0,
+    stagger: 1,
     duration: 2,
     ease: "expo.out",
   })
-  .from(".button", { opacity: 0, stagger: 1, duration: 2 });
-
-// gsap.to(".features", {
-//   // x: "400",
-//   duration: 3,
-//   scrollTrigger: {
-//     trigger: ".group22",
-//     start: " top 50%",
-//     end: "100 20%",
-//     scrub: 4,
-//     scrollActions: "restart none none none",
-//     pin: ".contents",
-//     markers: {
-//       startColor: "blue",
-//       fontSize: "2rem",
-//     },
-//   },
-// });
+  .from(".group22", { opacity: 0, x: "-10rem", duration: 2, ease: "expo.out" })
+  .from(".features-sub", {
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    ease: "expo.out",
+  })
+  .from("#contents-sub", {
+    opacity: 0,
+    y: "0rem",
+    duration: 1,
+    ease: "expo.out",
+  })
+  .from(".content-wrapper", {
+    opacity: 0,
+    y: "10rem",
+    duration: 2,
+    stagger: 1,
+    ease: "expo.out",
+  });
 
 let showMenu = false;
 
